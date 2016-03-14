92bd931e3c4f32323e6fd87f2d859e3858f97b76
lonnyxue <yunlong167167@163.com>
2016-03-14 16:47:45 +0800

    adfasdfasdf

--- test/src/tes2/Test2.java
+++ test/src/tes2/Test2.java
@@ -2,6 +2,6 @@ package tes2;
 
 public class Test2 {
 	public static void main(String[] args) {
-		System.out.println("a");
+		System.out.println("abbb");
 	}
 }
--- test/src/test/STrin.java
+++ test/src/test/STrin.java
@@ -2,6 +2,6 @@ package test;
 
 public class STrin {
 public static void main(String[] args) {
-	
+	System.out.println("a1");
 }
 }
