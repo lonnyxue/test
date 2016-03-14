b6874b2cb4ee63ceab6b0f79569bbe3f0ba62aea
lonnyxue <yunlong167167@163.com>
2016-03-14 17:15:06 +0800

    12345678

--- test/src/tes2/Test2.java
+++ test/src/tes2/Test2.java
@@ -2,6 +2,8 @@ package tes2;
 
 public class Test2 {
 	public static void main(String[] args) {
-		System.out.println("a4");
+		System.out.println("a41");
+		System.out.println("a41");
+		System.out.println("a41");
 	}
 }
